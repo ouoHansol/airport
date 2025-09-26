ef569b31338fa911cc8a22af10b19cf94f67a4a8
ouoHansol <rion_o_u@naver.com>
2025-09-26 13:49:24 +0900

    tap

--- app/components/layouts/tap/Tap.vue
+++ app/components/layouts/tap/Tap.vue
@@ -1,16 +1,7 @@
 <template>
-    <div class="tap">
+    <div v-for="tap in taps" :key="tap.path" class="tap" :class="{ 'active': tap.active }">
         <div class="tap-left-text">
-            계획관리
-        </div>
-        <div style="cursor: pointer;" class="tap-left-close">
-            X
-        </div>
-    </div>
-    
-    <div style="border-bottom: var(--color-primary) 1px solid;" class="tap">
-        <div class="tap-left-text">
-            비행검사 질의 게시판
+            {{ tap.name }}
         </div>
         <div style="cursor: pointer;" class="tap-left-close">
             X
@@ -19,6 +10,43 @@
 </template>
 
 <script setup lang="ts">
+
+const taps = ref<Tap[]>([
+    {
+        name: '계획관리',
+        path: '/plan',
+        active: false
+    },
+    {
+        name: '비행검사관리',
+        path: '/flight-result',
+        active: true
+    },
+    {
+        name: '비행검사 데이터이력',
+        path: '/flight-history',
+        active: false
+    },
+    {
+        name: '질의게시판',
+        path: '/qna',
+        active: false
+    },
+]);
+
+interface Tap {
+    name: string;
+    path: string;
+    active: boolean;
+}
+
+const props = defineProps({
+    tap_name: {
+        type: String,
+        default: ''
+    }
+});
+
 </script>
 
 <style scoped>
@@ -27,7 +55,7 @@
     min-height: 3rem;
     border-right: 1px solid var(--color-primary);
     border-top: 5px solid var(--color-primary);
-    border-bottom: 1px solid var(--color-white);
+    border-bottom: 1px solid var(--color-primary);
 
     display: flex;
     align-items: center;
@@ -42,5 +70,9 @@
     padding-right: 1rem;
     font-size: 0.8rem;
     font-weight: bold;
+}
+.active {
+    color: var(--color-active);
+    border-bottom: 1px solid var(--color-white);
 }
 </style>
